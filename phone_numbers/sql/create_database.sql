ef645a566438ff914797fad4e2e589c813ca7bcc
Krzysztof <krzysztof@student.edu>
2019-12-21 09:33:36 +0100

    Utworzenie projektu

---
+++ phone_numbers/sql/create_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database.
+
+Note: ONLY development database;
+*/
+
+CREATE USER phone_numbers SUPERUSER;
+CREATE DATABASE phone_numbers OWNER phone_numbers ENCODING 'utf-8';
---
+++ phone_numbers/sql/drop_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to drop development database;
+
+Note: ONLY development database;
+*/
+
+DROP DATABASE phone_numbers;
+DROP USER phone_numbers;
